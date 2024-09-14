0eae9da773bf8902d906f668f37c7a831f8e6562
Piotr Mroczkowski <p.mrocz@vp.pl>
2024-09-14 14:27:00 +0200

    Face detect

--- DetectFace/DetectFace.py
+++ DetectFace/DetectFace.py
@@ -7,13 +7,33 @@ capture = cv2.VideoCapture(camera)
 heightWindow = 480
 widthWindow = 640
 frameName = 'My camera'
+rgbZielony = (0, 128, 0)
+thickness = 5
+
 
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, heightWindow)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, widthWindow)
 
+faceDetector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
 while True:
     # capture.read() returns 2 values: first value from video; second just picture from camera
     _, img = capture.read()
+    
+    # reactreate frame but only in gray colors
+    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # face detection
+    faces = faceDetector.detectMultiScale(
+        grayImg,
+        scaleFactor = 1.2,
+        minNeighbors = 4,
+        minSize = (50, 50)
+    )
+    
+    for x, y, face_width, face_height in faces:
+        cv2.rectangle(img, (x, y), (x + face_width, y + face_height), rgbZielony, thickness)
+
     cv2.imshow(frameName, img)
     
     timeout = 50;
