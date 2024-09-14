986be452a128a0aad93c1e1961fda234c4a03759
Piotr Mroczkowski <p.mrocz@vp.pl>
2024-09-14 13:56:22 +0200

    Close frame on escape or X button

--- DetectFace/DetectFace.py
+++ DetectFace/DetectFace.py
@@ -6,6 +6,7 @@ capture = cv2.VideoCapture(camera)
 
 heightWindow = 480
 widthWindow = 640
+frameName = 'My camera'
 
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, heightWindow)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, widthWindow)
@@ -13,14 +14,14 @@ capture.set(cv2.CAP_PROP_FRAME_WIDTH, widthWindow)
 while True:
     # capture.read() returns 2 values: first value from video; second just picture from camera
     _, img = capture.read()
-    cv2.imshow('My picure', img)
+    cv2.imshow(frameName, img)
     
     timeout = 50;
     key = cv2.waitKey(timeout)
     
     escapeKeyValue = 27;
 
-    if key == escapeKeyValue:
+    if key == escapeKeyValue or cv2.getWindowProperty(frameName, cv2.WND_PROP_VISIBLE) < 1:
         break
 
 # release values
